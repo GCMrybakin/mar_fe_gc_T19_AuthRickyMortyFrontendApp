210ac6b6061f5461077a066722362499d65895f8
GCMrybakin <135844963+GCMrybakin@users.noreply.github.com>
2023-07-23 21:17:13 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'T18-Mrybakin-Angular';
+  title = 'T19-Mrybakin-Angular';
 }
